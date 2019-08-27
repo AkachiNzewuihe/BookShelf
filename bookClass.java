4cdc88778168afea70f4f39b86a3d1888fd9f1cb
Nzewuihe Akachi <akachinzewuihe@gmail.com>
2019-08-26 17:13:54 -0700

    Just added an update

---
+++ bookClass.java
@@ -0,0 +1,36 @@
+public class boook{
+private String bookTitle;
+private String bookCategory;
+private String bookAuthor;
+private String dateOfpublication;
+
+public static void main(String[] args){
+
+}
+
+public void setBooktitle(String bookTitle){
+    this.bookTitle = bookTitlee;
+}
+public void setBookauthor(String bookAuthor){
+    this.bookAuthor = bookAuthor;
+}
+public void setDateofPublication(String dateOfPublication){
+    this.dateOfPublication = dateOfPublication;
+}
+public void setBookcategory(String bookCategory){
+    this.bookCategory = bookCategory;
+}
+
+public String getBookname(){
+    return this.bookName
+}
+public String getBookauthor(){
+    return this.bookAuthor;
+}
+public String getdateOfPublication(){
+    return this.dateOfPublication;
+}
+public String getBookcategory(){
+    return this.bookCategory;
+}
+}
